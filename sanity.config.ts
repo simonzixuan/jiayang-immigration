c2b5d8277e90c083d1202c5e40f02df9dc9daa73
simonzixuan <zhengzixuanzhang@gmail.com>
2026-05-10 18:53:44 -0700

    Revert studio basePath

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   title: "佳阳移民后台",
   projectId: "byyhmzdj",
   dataset: "production",
-  basePath: "/studio",
   plugins: [structureTool()],
   schema: { types: schemaTypes },
 })
