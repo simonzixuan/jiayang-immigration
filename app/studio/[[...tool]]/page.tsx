d24db5d5b50945ab8eb82016bcadf41a36486914
simonzixuan <zhengzixuanzhang@gmail.com>
2026-05-10 18:10:30 -0700

    Fix studio SSR: split client component for NextStudio

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,8 +1,7 @@
-import { NextStudio } from "next-sanity/studio"
-import config from "../../../sanity.config"
+import StudioClient from "./StudioClient"
 
 export { metadata, viewport } from "next-sanity/studio"
 
 export default function StudioPage() {
-  return <NextStudio config={config} />
+  return <StudioClient />
 }
