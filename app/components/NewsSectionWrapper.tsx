518889821d0794dc496c1bc997c08b78852a5544
simonzixuan <zhengzixuanzhang@gmail.com>
2026-05-10 18:06:46 -0700

    Fix NewsSectionWrapper: remove unused lang prop

--- app/components/NewsSectionWrapper.tsx
+++ app/components/NewsSectionWrapper.tsx
@@ -1,11 +1,9 @@
 "use client"
 
-import { useLang } from "../context/lang"
 import dynamic from "next/dynamic"
 
 const NewsSection = dynamic(() => import("./NewsSection"), { ssr: false })
 
 export default function NewsSectionWrapper() {
-  const { lang } = useLang()
-  return <NewsSection lang={lang} />
+  return <NewsSection />
 }
